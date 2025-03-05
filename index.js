455a83cad8054a47a97aa3123d318dba3e7b2d02
dagger983 <kmvignesh1406@gmail.com>
2025-03-05 21:24:23 +0530

    first commit

--- index.js
+++ index.js
@@ -12,13 +12,7 @@ const port = 33060;
 const cors = require("cors");
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || ["https://jasandyas.netlify.app"].includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true 
