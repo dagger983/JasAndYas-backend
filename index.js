d4927e271354c70091a7333c12e83de80f45528a
dagger983 <kmvignesh1406@gmail.com>
2025-03-18 13:26:08 +0530

    first commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.options("*", cors(corsOptions));
 app.use(cors(corsOptions));
 
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.MYSQL_ADDON_HOST,
   user: process.env.MYSQL_ADDON_USER,
   password: process.env.MYSQL_ADDON_PASSWORD,
